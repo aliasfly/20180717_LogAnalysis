9237c777ce1bccf290b4a38689bf769aee6f15b2
aliasfly <zhangzexiangks@126.com>
2018-07-17 09:37:00 +0800

    window as root

--- LogAnalysis.pro
+++ LogAnalysis.pro
@@ -1,13 +1,18 @@
 TEMPLATE = app
 
-QT += qml quick charts core
+QT += quick charts qml core
 CONFIG += c++11
 
 # The following define makes your compiler emit warnings if you use
 # any feature of Qt which as been marked deprecated (the exact warnings
 # depend on your compiler). Please consult the documentation of the
 # deprecated API in order to know how to port your code away from it.
+
 DEFINES += QT_DEPRECATED_WARNINGS
+
+QMAKE_CXXFLAGS +=-Wno-reorder -Wno-unused-parameter -Wno-ignored-qualifiers
+
+QMAKE_CXX=g++
 
 # You can also make your code fail to compile if you use deprecated APIs.
 # In order to do so, uncomment the following line.
--- main.cpp
+++ main.cpp
@@ -1,28 +1,29 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
-#include <QQuickView>
-#include <QtQml>
 #include "fileparse.h"
+
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
+
+
     QQmlApplicationEngine engine;
-
-    QQuickView viewer;
-
-    QObject::connect(viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));//quit_use
-
-    viewer.rootContext()->setContextProperty("qml_fileParse", new FileParse);
-
-    viewer.setSource(QUrl("qrc:///main.qml"));
-    viewer.setResizeMode(QQuickView::SizeRootObjectToView);//default
-    viewer.show();
-
-//    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-//    if (engine.rootObjects().isEmpty())
-//        return -1;
+    engine.load(QUrl(QLatin1String("qrc:///main.qml")));
+    if (engine.rootObjects().isEmpty())
+        return -1;
 
     return app.exec();
+
+//    QQuickView viewer;
+
+//    QObject::connect(viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));//quit_use
+
+//    viewer.rootContext()->setContextProperty("qml_fileParse", new FileParse);
+//    viewer.setSource(QUrl("qrc:///main.qml"));
+//    viewer.setResizeMode(QQuickView::SizeRootObjectToView);//default
+//    viewer.show();
+
+//    return app.exec();
 }
