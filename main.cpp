2c7c31202c01eb3a074d488ee239ac02b1fb8070
aliasfly <zhangzexiangks@126.com>
2018-07-17 13:30:41 +0800

    main.cpp OK applicationwindow

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
+#include <QtQml>
 #include "fileparse.h"
 
 
@@ -8,22 +9,11 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
 
 
-
     QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("qml_fileParse", new FileParse);
     engine.load(QUrl(QLatin1String("qrc:///main.qml")));
     if (engine.rootObjects().isEmpty())
         return -1;
 
     return app.exec();
-
-//    QQuickView viewer;
-
-//    QObject::connect(viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));//quit_use
-
-//    viewer.rootContext()->setContextProperty("qml_fileParse", new FileParse);
-//    viewer.setSource(QUrl("qrc:///main.qml"));
-//    viewer.setResizeMode(QQuickView::SizeRootObjectToView);//default
-//    viewer.show();
-
-//    return app.exec();
 }
